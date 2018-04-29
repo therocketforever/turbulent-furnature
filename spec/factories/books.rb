c010cabfe00af768916640b7781619620f601c3a
Don Wei <therocketforever@me.com>
2018-04-29 15:56:41 -0400

    seed some randomness in test titles to avoid collisions

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :book do
     format { Format.count === 0 ? build( :format ) : Format.all.sample }
 
-    title { Faker::RuPaul.quote  }
+    title { "#{ Faker::RuPaul.quote } #{ Digest::SHA256.hexdigest( SecureRandom.random_bytes )[ 0...7 ] }"}
     release { Time.now }
     price { 10.00 }
   end
